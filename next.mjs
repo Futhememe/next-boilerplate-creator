835798a518daa9f74e15f1672d53c5db35046665
Futhememe <kyoro_souza@hotmail.com>
2022-04-12 10:06:39 -0300

    🎨 new commands to create a _app.tsx file

--- next.mjs
+++ next.mjs
@@ -2,6 +2,8 @@
 
 let projectName = await question("What's the project name ?: ")
 
+let appContent = await fs.readFile('./templates/_app.tsx')
+
 cd('..')
 
 await $`yarn create next-app --typescript ${projectName}`
@@ -16,3 +18,10 @@ await $`mv styles src`
 await $`yarn add @chakra-ui/react @emotion/react@^11 @emotion/styled@^11 framer-motion@^6`
 
 await $`yarn add react-i18next i18next`
+
+cd('src')
+cd('pages')
+
+await $`rm _app.tsx`
+
+await fs.writeFile('_app.tsx', appContent)
