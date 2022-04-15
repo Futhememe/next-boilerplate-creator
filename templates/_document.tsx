d74954e591a3fecc6af292d7af2c2ce41d48a896
Futhememe <kyoro_souza@hotmail.com>
2022-04-15 13:49:20 -0300

    🎨 update document template

--- templates/_document.tsx
+++ templates/_document.tsx
@@ -5,7 +5,11 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+        </Head>
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
           <ColorModeScript />
