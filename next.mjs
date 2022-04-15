ecb1de34496da850aacbcef1915385d38a5697fe
Futhememe <kyoro_souza@hotmail.com>
2022-04-15 13:49:06 -0300

    🚧 new commands to create the boilerplate

--- next.mjs
+++ next.mjs
@@ -5,6 +5,8 @@ let projectName = await question("What's the project name ?: ")
 let appContent = await fs.readFile('./templates/_app.tsx')
 let themeContent = await fs.readFile('./templates/theme.tsx')
 let documentContent = await fs.readFile('./templates/_document.tsx')
+let containerContent = await fs.readFile('./templates/Container.tsx')
+let indexContent = await fs.readFile('./templates/index.tsx')
 
 cd('..')
 
@@ -15,7 +17,12 @@ cd(projectName)
 await $`mkdir src`
 
 await $`mv pages src`
-await $`mv styles src`
+
+cd('styles')
+await $`rm globals.css Home.module.css`
+
+cd('..')
+await $`rmdir styles`
 
 await $`yarn add @chakra-ui/react @emotion/react@^11 @emotion/styled@^11 framer-motion@^6`
 
@@ -24,10 +31,20 @@ await $`yarn add react-i18next i18next`
 cd('src')
 await fs.writeFile('theme.tsx', themeContent)
 
+await $`mkdir components`
+
+cd('components')
+
+await fs.writeFile('Container.tsx', containerContent)
+
+cd('..')
+
 cd('pages')
 
 await fs.writeFile('_document.tsx', documentContent)
 
 await $`rm _app.tsx`
+await $`rm index.tsx`
 
 await fs.writeFile('_app.tsx', appContent)
+await fs.writeFile('index.tsx', indexContent)
